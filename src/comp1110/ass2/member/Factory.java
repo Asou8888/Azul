6fa115001d2ea9bfdcadd813e3e2e5c36a884918
YIxin Ge <u6616522@anu.edu.au>
2021-03-27 18:27:16 +0800

    Factory and Storage

---
+++ src/comp1110/ass2/member/Factory.java
@@ -0,0 +1,69 @@
+package comp1110.ass2.member;
+
+public class Factory {
+    /**
+     * Factory is made of 4 tiles.
+     */
+    private Tile[] tiles = new Tile[4];
+
+    /**
+     * Constructor for the Factory. Given array of tiles, returns the
+     * current state of where each char represented tile is location in the Factory.
+     *
+     * @param tiles the given array of Tile
+     */
+    public Factory(Tile[] tiles){
+        //TODO
+    }
+
+    /**
+     * Return the code of the current state in Factory.
+     * @return the String code of Factory.
+     */
+    public String getCode() {
+        return encode();
+    }
+
+    /**
+     * The Factories placement string begins with an 'F' and is followed by a number of individual factory placement string.
+     * Each encoding of a singular Factory placement string is as follows:
+     *
+     * {Factory number}{tiles}
+     *
+     * The first character is a number 0-8 representing the Factory number. Factories are numbered sequentially, so in a 2-player
+     * game, we will have factories 0 to 4.
+     * The following 0 - 4 characters are letters a to e representing the tiles stored there in alphabetical order.
+     * For example: "F0abbe2ccdd" reads "Factory 0 has one a tile, two b tiles, one c tile, one e tile, Factory 2
+     * contains two c tiles and two d tiles, and Factories 1, 3, and 4 are empty.
+     * If a factory is empty, it does not appear in the factories string.
+     *
+     * The number of factories depends on the number of players.
+     *
+     * 2 players: 5 factories
+     * 3 players: 7 factories
+     * 4 players: 9 factories
+     */
+    private String encode() {
+        //TODO
+        return "";
+    }
+
+    /**
+     * Determines if the factory currently has no tiles.
+     * @return whether the factory currently has no tiles.
+     */
+    public boolean isEmpty(){
+        //TODO
+        return false;
+    }
+
+    /**
+     * find the amount of the tiles in a Factory
+     * @return the amount of tiles in a Factory
+     */
+    public int tileAmount(){
+        //TODO
+        return 0;
+    }
+
+}
---
+++ src/comp1110/ass2/member/Storage.java
@@ -0,0 +1,80 @@
+package comp1110.ass2.member;
+
+public class Storage {
+    /**Storage has 5 rows and it is a triangular array of tiles */
+    private Tile[] tiles = new Tile[15];
+
+    /**
+     * Constructor for the Storage. Given an array of tiles, returns the
+     * current state of where each char represented tile is location in the storage.
+     *
+     * @param tiles the given array of Tile
+     */
+    public Storage(Tile[] tiles){
+        //TODO
+    }
+
+    /**
+     * Return the code of the current state in Storage.
+     * @return the String code of Storage.
+     */
+    public String getCode() {
+        return encode();
+    }
+
+    /**
+     * The Storage String is composed of 3-character substrings representing the tiles in the storage area. These are ordered
+     * numerically by row number.
+     *
+     * The first character is "S"
+     *
+     * The remaining characters are in groups of three in the pattern: {row number}{tile}{number of tiles}
+     *
+     * row number 0 - 4.
+     * tile a - e.
+     * number of tiles stored in that row from 1 - 5.
+     * The maximum number of tiles in a given row is equal to (row number + 1).
+     * @return a String of code
+     */
+    private String encode() {
+        //TODO
+        return "";
+    }
+
+    /**
+     * Determines if the tiles are filled fully in one row.
+     * @return whether a row is filled by tiles fully.
+     */
+    public boolean isRowFull(){
+        //TODO
+        return false;}
+
+    /**
+     * Determines if the color of tiles got from factory is same
+     * as the tiles in a row in Storage.
+     * @return whether the color of tiles from factory and currently
+     * in a row in Storage are same.
+     */
+    public boolean isRowColorSame(){
+        //TODO
+        return false;}
+
+    /**
+     * Determines the color of tiles from factory is not same as any
+     * one of colors in the same row in Mosaic.
+     * @return whether the color is not same as any colors having in the same row in Mosaic.
+     */
+    public boolean isColorDifInMosaicRow(){
+        //TODO
+        return false;}
+
+    /**
+     * Determines if the rightmost space for tile in a row is empty.
+     * @return whether the rightmost space for tile in a row is empty or not.
+     */
+    public  boolean isRightEmpty(){
+        //TODO
+        return false;
+    }
+
+}
