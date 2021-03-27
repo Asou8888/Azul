b09e1964eeb3b607e2322fd0957eaef05aafce0d
YIxin Ge <u6616522@anu.edu.au>
2021-03-27 19:36:16 +0800

    tile

--- src/comp1110/ass2/member/Tile.java
+++ src/comp1110/ass2/member/Tile.java
@@ -3,38 +3,49 @@ package comp1110.ass2.member;
 public class Tile {
     /* The maximum length of any valid combination of tiles is 100 */
     public static final int MAX_NUM = 100;
+    public static final int MAX_First = 1;
+
     char colorChar;
+    String colorName;
+    //
+    public String getCode() {
+        return String.valueOf(colorChar);
+    }
 }
 
 class BlueTile extends Tile{
-         public BlueTile(char colorChar) {
-             colorChar = 'a';
-             super.colorChar = colorChar;
+         public BlueTile() {
+             super.colorChar = 'a';
+             super.colorName = "blue";
          }
 }
 class GreenTile extends Tile{
-    public GreenTile(char colorChar) {
-        colorChar = 'b';
-        super.colorChar = colorChar;
+    public GreenTile() {
+        super.colorChar = 'b';
+        super.colorName = "green";
     }
 }
 class OrangeTile extends Tile{
     public OrangeTile(char colorChar) {
-        colorChar = 'c';
-        super.colorChar = colorChar;
+        super.colorChar = 'c';
+        super.colorName = "orange";
     }
 }
 class PurpleTile extends Tile{
-    public PurpleTile(char colorChar) {
-        colorChar = 'd';
-        super.colorChar = colorChar;
+    public PurpleTile() {
+        super.colorChar = 'd';
+        super.colorName = "purple";
     }
 }
 class RedTile extends Tile{
-    public RedTile(char colorChar) {
-        colorChar = 'e';
-        super.colorChar = colorChar;
+    public RedTile() {
+        super.colorChar = 'e';
+        super.colorName = "red";
     }
 }
-
+class FirstPlayer extends Tile{
+    public FirstPlayer() {
+        super.colorChar = 'f';
+        super.colorName = "firstPlayer";
     }
+}
