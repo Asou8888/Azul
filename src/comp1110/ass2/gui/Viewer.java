c2cf3c7fe32ff4fa0cd76de8954e1c565aa8204f
Xiao Xu <u7205634@anu.edu.au>
2021-04-18 21:00:15 +0800

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	src/comp1110/ass2/Azul.java

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -26,17 +26,6 @@ public class Viewer extends Application {
     private TextField playerTextField;
     private TextField boardTextField;
 
-    /*
-     * Some added members:
-     *     1. displays: The field displays shared state(factories, centers, bag, discard) and player state(storage, mosaic, etc)
-     *     2. sharedField: the field displays shared field(in the 'displays').
-     *     3. playerField: the field displays player field(in the 'display').
-     */
-    private static final int FACTORIES_NUM = 5;
-    private final Group displays  = new Group();
-    private final Group sharedField = new Group();
-    private final Group playerField = new Group();
-
 
     /**
      * Draw a placement in the window, removing any previously drawn placements
@@ -45,48 +34,219 @@ public class Viewer extends Application {
      *              TASK 4
      */
     void displayState(String[] state) {
-        // FIXME Task 4: implement the simple state viewer
+        /**
+         -------- 1-5 sharedState --------
+         1. String turnRound
+         2. String Factories   2.1-2.5 factory0 factory1 factory2 factory3 factory4 (null if factory is empty)
+         3. String Center
+         4. String Bag
+         5. String Discard
+         --------  6-10 Player A --------
+         6. String APLayer  (the whole state of A)
+         7. Int Ascore  (score of A)
+         8. String Amosaic  (code of mosaic of A)
+         9. String Astorage  (code of storage of A)
+         10. String Afloor  (code of floor of A)
+         -------- 11-15 Player B --------
+         11. String BPlayer
+         12. int Bscore
+         13. String Bmosaic
+         14. String Bstorage
+         15. String Bfloor
 
-        /*  Code written by Ruizheng Shen:
-         *     1. Desperate the area for shared field and player field.
-         *     2. the simple shared field.
+         written by Xiao Xu
          */
-        Label sharedLabel = new Label("shared state: ");
-        ArrayList<HBox> sharedLabels = new ArrayList<>();
-        TextField[] factories = new TextField[FACTORIES_NUM];
-        Label[] facLabels = new Label[FACTORIES_NUM];
-        for (int i = 0; i < FACTORIES_NUM; i++) {
-            factories[i] = new TextField("factory [" + i + "] code"); // TODO: put the i-th factory code here.
-            facLabels[i] = new Label("factory [" + i + "]: ");
-        }
-        TextField center = new TextField("center code"); // TODO: put the center code here.
-        Label centerLabel = new Label("Center: ");
-        TextField bag = new TextField("bag code"); // TODO: put the bag code here.
-        Label bagLabel = new Label("Bag: ");
-        TextField discard = new TextField("discard code"); // TODO: put the discard code here.
-        Label discardLabel = new Label("Discard: ");
+        String sharedState = state[0];
+        String playerState = state[1];
+        /**
+         * 1-5 sharedState
+         **/
+        String turnRound = sharedState.substring(0,1);
+        int Fac = 0;
+        int Cen = 0;
+        int B = 0;
+        int Dis = 0;
+        for (int i = 0; i < sharedState.length();i++){
+            if(sharedState.toCharArray()[i] == 'F'){
+                Fac = i;
+            }
+            if(sharedState.toCharArray()[i] == 'C'){
+                Cen = i;
 
-        // adding horizon box for factories
-        for (int i = 0; i < FACTORIES_NUM; i++)
-        sharedLabels.add(new HBox(facLabels[i], factories[i]));
-        // adding horizon box for center
-        sharedLabels.add(new HBox(centerLabel, center));
-        // adding horizon box for bag
-        sharedLabels.add(new HBox(bagLabel, bag));
-        // adding horizon box for discard
-        sharedLabels.add(new HBox(discardLabel, discard));
-
-        VBox vb = new VBox();
-        vb.setSpacing(5);
-        vb.setLayoutX(50);
-        vb.setLayoutY(10);
-        vb.getChildren().add(sharedLabel);
-        for (HBox hr : sharedLabels) {
-            vb.getChildren().add(hr);
+            }
+            if(sharedState.toCharArray()[i] == 'B'){
+                B = i;
+            }
+            if(sharedState.toCharArray()[i] == 'D'){
+                Dis = i;
+            }
         }
-        sharedField.getChildren().add(vb);
-        displays.getChildren().add(sharedField);
-    }
+
+        String Factories = sharedState.substring(Fac,Cen);
+        String Center = sharedState.substring(Cen,B);
+        String Bag = sharedState.substring(B,Dis);
+        String Discard = sharedState.substring(Dis);
+
+        String factory0 = "";
+        String factory1 = "";
+        String factory2 = "";
+        String factory3 = "";
+        String factory4 = "";
+
+        String numberOfFactories = "";
+        if(Factories.trim() != null && !"".equals(Factories.trim())) {
+            for (int i = 0; i < Factories.trim().length(); i++) {
+                if (Factories.trim().charAt(i) >= 48 && Factories.trim().charAt(i) <= 57) {
+                    numberOfFactories += Factories.trim().charAt(i);
+                }
+            }
+        }
+        int[] indexList = new int[numberOfFactories.length()];
+        for(int i = 0; i < indexList.length;i++){
+            for(int n = 0;n < Factories.length();n++){
+                if(Factories.toCharArray()[n] == numberOfFactories.toCharArray()[i]){
+                    indexList[i] = n;
+                }
+            }
+        }
+
+        String[] breakFactories = new String[indexList.length];
+        for(int i = 0; i < indexList.length-1;i++){
+            breakFactories[i] = Factories.substring(indexList[i],indexList[i+1]);
+        }
+        breakFactories[indexList.length-1] = Factories.substring(indexList[indexList.length-1]);
+
+        for(int i = 0;i<breakFactories.length;i++){
+            if(breakFactories[i].toCharArray()[0] == '0'){
+                factory0 = breakFactories[i];
+            }
+            if(breakFactories[i].toCharArray()[0] == '1'){
+                factory1 = breakFactories[i];
+            }
+            if(breakFactories[i].toCharArray()[0] == '2'){
+                factory2 = breakFactories[i];
+            }
+            if(breakFactories[i].toCharArray()[0] == '3'){
+                factory3 = breakFactories[i];
+            }
+            if(breakFactories[i].toCharArray()[0] == '4'){
+                factory4 = breakFactories[i];
+            }
+        }
+        if(factory0 == ""){
+            factory0 = "null";
+        }
+        if(factory1 == ""){
+            factory1 = "null";
+        }
+        if(factory2 == ""){
+            factory2 = "null";
+        }
+        if(factory3 == ""){
+            factory3 = "null";
+        }
+        if(factory4 == ""){
+            factory4 = "null";
+        }
+
+
+
+        /**
+         * 6-15 playerState
+         **/
+        int BplayerIndex = 0;
+        for (int i = 0; i < playerState.length(); i++) {
+            if (playerState.toCharArray()[i] == 'B') {
+                BplayerIndex = i;
+            }
+
+        }
+        String APlayer = playerState.substring(0, B);
+        String BPlayer = playerState.substring(B);
+
+        //player A
+        int Ma = 0; //index of mosaic of A
+        int Sa = 0; //index of storage of A
+        int Fa = 0; //index of floor of A
+        for (int i = 0; i < APlayer.length(); i++) {
+            if (APlayer.toCharArray()[i] == 'M') {
+                Ma = i;
+            }
+            if (APlayer.toCharArray()[i] == 'S') {
+                Sa = i;
+            }
+            if (APlayer.toCharArray()[i] == 'F') {
+                Fa = i;
+            }
+        }
+        int Ascore = Integer.parseInt(APlayer.substring(1, Ma));
+        String Amosaic = APlayer.substring(Ma, Sa);
+        String Astorage = APlayer.substring(Sa, Fa);
+        String Afloor = APlayer.substring(Fa);
+
+
+        //player B
+        int Mb = 0; //index of mosaic of B
+        int Sb = 0; //index of storage of B
+        int Fb = 0; //index of floor of B
+        for (int i = 0; i < BPlayer.length(); i++) {
+            if (BPlayer.toCharArray()[i] == 'M') {
+                Mb = i;
+            }
+            if (BPlayer.toCharArray()[i] == 'S') {
+                Sb = i;
+            }
+            if (BPlayer.toCharArray()[i] == 'F') {
+                Fb = i;
+            }
+        }
+        int Bscore = Integer.parseInt(BPlayer.substring(1, Mb));
+        String Bmosaic = BPlayer.substring(Mb, Sb);
+        String Bstorage = BPlayer.substring(Sb, Fb);
+        String Bfloor = BPlayer.substring(Fb);
+
+            // FIXME Task 4: implement the simple state viewer
+
+            /*  Code written by Ruizheng Shen:
+             *     1. Desperate the area for shared field and player field.
+             *     2. the simple shared field.
+             */
+            Label sharedLabel = new Label("shared state: ");
+            ArrayList<HBox> sharedLabels = new ArrayList<>();
+            TextField[] factories = new TextField[FACTORIES_NUM];
+            Label[] facLabels = new Label[FACTORIES_NUM];
+            for (int i = 0; i < FACTORIES_NUM; i++) {
+                factories[i] = new TextField("factory [" + i + "] code"); // TODO: put the i-th factory code here.
+                facLabels[i] = new Label("factory [" + i + "]: ");
+            }
+            TextField center = new TextField("center code"); // TODO: put the center code here.
+            Label centerLabel = new Label("Center: ");
+            TextField bag = new TextField("bag code"); // TODO: put the bag code here.
+            Label bagLabel = new Label("Bag: ");
+            TextField discard = new TextField("discard code"); // TODO: put the discard code here.
+            Label discardLabel = new Label("Discard: ");
+
+            // adding horizon box for factories
+            for (int i = 0; i < FACTORIES_NUM; i++)
+                sharedLabels.add(new HBox(facLabels[i], factories[i]));
+            // adding horizon box for center
+            sharedLabels.add(new HBox(centerLabel, center));
+            // adding horizon box for bag
+            sharedLabels.add(new HBox(bagLabel, bag));
+            // adding horizon box for discard
+            sharedLabels.add(new HBox(discardLabel, discard));
+
+            VBox vb = new VBox();
+            vb.setSpacing(5);
+            vb.setLayoutX(50);
+            vb.setLayoutY(10);
+            vb.getChildren().add(sharedLabel);
+            for (HBox hr : sharedLabels) {
+                vb.getChildren().add(hr);
+            }
+            sharedField.getChildren().add(vb);
+            displays.getChildren().add(sharedField);
+        }
 
     /**
      * Create a basic text field for input and a refresh button.
