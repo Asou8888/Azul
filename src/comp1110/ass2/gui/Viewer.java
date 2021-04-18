7c90a1cdd555142f81d6bb46d61b9df3983ff6b6
Xiao Xu <u7205634@anu.edu.au>
2021-04-18 22:03:37 +0800

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	src/comp1110/ass2/Azul.java

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -115,28 +115,33 @@ public class Viewer extends Application {
             }
         }
 
-        String[] breakFactories = new String[indexList.length];
-        for(int i = 0; i < indexList.length-1;i++){
-            breakFactories[i] = Factories.substring(indexList[i],indexList[i+1]);
-        }
-        breakFactories[indexList.length-1] = Factories.substring(indexList[indexList.length-1]);
+        try {
+            String[] breakFactories = new String[indexList.length];
 
-        for(int i = 0;i<breakFactories.length;i++){
-            if(breakFactories[i].toCharArray()[0] == '0'){
-                factory0 = breakFactories[i];
+            for (int i = 0; i < indexList.length - 1; i++) {
+                breakFactories[i] = Factories.substring(indexList[i], indexList[i + 1]);
             }
-            if(breakFactories[i].toCharArray()[0] == '1'){
-                factory1 = breakFactories[i];
+            breakFactories[indexList.length - 1] = Factories.substring(indexList[indexList.length - 1]);
+
+            for (int i = 0; i < breakFactories.length; i++) {
+                if (breakFactories[i].toCharArray()[0] == '0') {
+                    factory0 = breakFactories[i];
+                }
+                if (breakFactories[i].toCharArray()[0] == '1') {
+                    factory1 = breakFactories[i];
+                }
+                if (breakFactories[i].toCharArray()[0] == '2') {
+                    factory2 = breakFactories[i];
+                }
+                if (breakFactories[i].toCharArray()[0] == '3') {
+                    factory3 = breakFactories[i];
+                }
+                if (breakFactories[i].toCharArray()[0] == '4') {
+                    factory4 = breakFactories[i];
+                }
             }
-            if(breakFactories[i].toCharArray()[0] == '2'){
-                factory2 = breakFactories[i];
-            }
-            if(breakFactories[i].toCharArray()[0] == '3'){
-                factory3 = breakFactories[i];
-            }
-            if(breakFactories[i].toCharArray()[0] == '4'){
-                factory4 = breakFactories[i];
-            }
+        }catch(Exception e){
+            Factories = "";
         }
         if(factory0 == ""){
             factory0 = "null";
@@ -166,8 +171,8 @@ public class Viewer extends Application {
             }
 
         }
-        String APlayer = playerState.substring(0, B);
-        String BPlayer = playerState.substring(B);
+        String APlayer = playerState.substring(0, BplayerIndex);
+        String BPlayer = playerState.substring(BplayerIndex);
 
         //player A
         int Ma = 0; //index of mosaic of A
@@ -210,48 +215,48 @@ public class Viewer extends Application {
         String Bstorage = BPlayer.substring(Sb, Fb);
         String Bfloor = BPlayer.substring(Fb);
 
-            // FIXME Task 4: implement the simple state viewer
+        // FIXME Task 4: implement the simple state viewer
 
-            /*  Code written by Ruizheng Shen:
-             *     1. Desperate the area for shared field and player field.
-             *     2. the simple shared field.
-             */
-            Label sharedLabel = new Label("shared state: ");
-            ArrayList<HBox> sharedLabels = new ArrayList<>();
-            TextField[] factories = new TextField[FACTORIES_NUM];
-            Label[] facLabels = new Label[FACTORIES_NUM];
-            for (int i = 0; i < FACTORIES_NUM; i++) {
-                factories[i] = new TextField("factory [" + i + "] code"); // TODO: put the i-th factory code here.
-                facLabels[i] = new Label("factory [" + i + "]: ");
-            }
-            TextField center = new TextField("center code"); // TODO: put the center code here.
-            Label centerLabel = new Label("Center: ");
-            TextField bag = new TextField("bag code"); // TODO: put the bag code here.
-            Label bagLabel = new Label("Bag: ");
-            TextField discard = new TextField("discard code"); // TODO: put the discard code here.
-            Label discardLabel = new Label("Discard: ");
-
-            // adding horizon box for factories
-            for (int i = 0; i < FACTORIES_NUM; i++)
-                sharedLabels.add(new HBox(facLabels[i], factories[i]));
-            // adding horizon box for center
-            sharedLabels.add(new HBox(centerLabel, center));
-            // adding horizon box for bag
-            sharedLabels.add(new HBox(bagLabel, bag));
-            // adding horizon box for discard
-            sharedLabels.add(new HBox(discardLabel, discard));
-
-            VBox vb = new VBox();
-            vb.setSpacing(5);
-            vb.setLayoutX(50);
-            vb.setLayoutY(10);
-            vb.getChildren().add(sharedLabel);
-            for (HBox hr : sharedLabels) {
-                vb.getChildren().add(hr);
-            }
-            sharedField.getChildren().add(vb);
-            displays.getChildren().add(sharedField);
+        /*  Code written by Ruizheng Shen:
+            *     1. Desperate the area for shared field and player field.
+            *     2. the simple shared field.
+            */
+        Label sharedLabel = new Label("shared state: ");
+        ArrayList<HBox> sharedLabels = new ArrayList<>();
+        TextField[] factories = new TextField[FACTORIES_NUM];
+        Label[] facLabels = new Label[FACTORIES_NUM];
+        for (int i = 0; i < FACTORIES_NUM; i++) {
+            factories[i] = new TextField("factory [" + i + "] code"); // TODO: put the i-th factory code here.
+            facLabels[i] = new Label("factory [" + i + "]: ");
         }
+        TextField center = new TextField("center code"); // TODO: put the center code here.
+        Label centerLabel = new Label("Center: ");
+        TextField bag = new TextField("bag code"); // TODO: put the bag code here.
+        Label bagLabel = new Label("Bag: ");
+        TextField discard = new TextField("discard code"); // TODO: put the discard code here.
+        Label discardLabel = new Label("Discard: ");
+
+        // adding horizon box for factories
+        for (int i = 0; i < FACTORIES_NUM; i++)
+            sharedLabels.add(new HBox(facLabels[i], factories[i]));
+        // adding horizon box for center
+        sharedLabels.add(new HBox(centerLabel, center));
+        // adding horizon box for bag
+        sharedLabels.add(new HBox(bagLabel, bag));
+        // adding horizon box for discard
+        sharedLabels.add(new HBox(discardLabel, discard));
+
+        VBox vb = new VBox();
+        vb.setSpacing(5);
+        vb.setLayoutX(50);
+        vb.setLayoutY(10);
+        vb.getChildren().add(sharedLabel);
+        for (HBox hr : sharedLabels) {
+            vb.getChildren().add(hr);
+        }
+        sharedField.getChildren().add(vb);
+        displays.getChildren().add(sharedField);
+    }
 
     /**
      * Create a basic text field for input and a refresh button.
@@ -291,8 +296,8 @@ public class Viewer extends Application {
 
         // test code by Ruizheng Shen(for testing displayState())
         String[] testStr = new String[] {
-            "adada",
-            "adadadadad"
+            "AF0cdde1bbbe2abde3cdee4bcceCfB1915161614D0000000000",
+            "A7Md03c13b23e32S4e1FefB3Ma00b12d20S3c2F"
         };
         displayState(testStr);
         root.getChildren().add(displays);
