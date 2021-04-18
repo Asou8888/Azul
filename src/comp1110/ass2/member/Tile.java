00642de9e9f106cc96d0432a0cc7ab075d7a7d72
asou <u6814863@anu.edu.au>
2021-04-19 02:25:04 +0800

    Update Tile

--- src/comp1110/ass2/member/Tile.java
+++ src/comp1110/ass2/member/Tile.java
@@ -5,53 +5,48 @@ package comp1110.ass2.member;
  * @version 1.0
  * @since 2021.3.27
  */
+
+/**
+ * Modified by Ruizheng Shen in 2021.4.18
+ *     1. Removing inherited classes.
+ *     2. Removing (String)'colorName' member.
+ *     3. Add a Enum class TileType, for the tile's color.
+ *     4. Add a constructor.
+ *     5. Add 'private' keyword to tileType and colorChar.
+ */
+
 public class Tile {
     /* The maximum length of any valid combination of tiles is 100 */
     /* The firstPlayer is a special tile */
     public static final int MAX_NUM = 100;
     public static final int MAX_First = 1;
 
-    char colorChar;
-    String colorName;
+    private final TileType tileType; // the color of this tile.
+
+    private char colorChar; // the code for this tile.(considering using a map from color name to code.)
+
+    // Constructor
+    Tile(TileType color) {
+        this.tileType = color;
+        switch (color) {
+            case Blue -> this.colorChar = 'a';
+            case Green -> this.colorChar = 'b';
+            case Orange -> this.colorChar = 'c';
+            case Purple -> this.colorChar = 'd';
+            case Red -> this.colorChar = 'e';
+            case FirstPlayer -> this.colorChar = 'f';
+        }
+    }
+
     /* Given a char from tile and returns in string form*/
     public String getCode() {
         return String.valueOf(colorChar);
     }
-}
 
-class BlueTile extends Tile{
-         public BlueTile() {
-             super.colorChar = 'a';
-             super.colorName = "blue";
-         }
-}
-class GreenTile extends Tile{
-    public GreenTile() {
-        super.colorChar = 'b';
-        super.colorName = "green";
+    /* the visitor to tileType, returning a enum type */
+    public TileType getTileType() {
+        return this.tileType;
     }
-}
-class OrangeTile extends Tile{
-    public OrangeTile(char colorChar) {
-        super.colorChar = 'c';
-        super.colorName = "orange";
-    }
-}
-class PurpleTile extends Tile{
-    public PurpleTile() {
-        super.colorChar = 'd';
-        super.colorName = "purple";
-    }
-}
-class RedTile extends Tile{
-    public RedTile() {
-        super.colorChar = 'e';
-        super.colorName = "red";
-    }
-}
-class FirstPlayer extends Tile{
-    public FirstPlayer() {
-        super.colorChar = 'f';
-        super.colorName = "firstPlayer";
-    }
+
+
 }
---
+++ src/comp1110/ass2/member/TileType.java
@@ -0,0 +1,15 @@
+package comp1110.ass2.member;
+
+/**
+ * @author Ruizheng Shen
+ * @version 1.0
+ * @since 2021.4.18
+ */
+public enum TileType {
+    Blue,
+    Green,
+    Orange,
+    Purple,
+    Red,
+    FirstPlayer
+}
